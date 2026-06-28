050f92dcf9bc2c7176d42e9ece23b7f2e271cc6f
tejaskale99 <tejaskale1556@gmail.com>
2026-06-28 14:21:44 +0530

    feat:- add LoginDto

---
+++ src/modules/auth/dto/login.dto.js
@@ -0,0 +1,10 @@
+import BaseDto from '../../../common/dto/baseDto.js'
+import joi from 'joi'
+
+class LoginDto extends BaseDto{
+    static schema = joi.object({
+        email: joi.string().email().lowercase().trim().required(),
+        password: joi.string().min(8).required().message('Password must be at least 8 characters long and not exceed 50 characters'),
+    })
+}
+export default LoginDto;
